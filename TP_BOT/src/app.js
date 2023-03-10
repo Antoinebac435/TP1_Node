786024de25b82be6a6c34743f70f6d8edae1ddb3
Antoinebac435 <antoinebacquet62@gmail.com>
2023-03-10 16:22:17 +0100

    Commandes

--- TP_BOT/src/app.js
+++ TP_BOT/src/app.js
@@ -1,23 +1,54 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const conf = require('../conf.json');
-const TOKEN = conf.token;
-
-// Créer un nouveau client
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-const { Events } = require('discord.js');
-
-module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
-    },
-};
-
 const fs = require('node:fs');
 const path = require('node:path');
+const TOKEN = conf.token;
 
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(
+            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(
+            `Aucune commande ${interaction.commandName} n'a été trouvée.`
+        );
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'Erreur dans l\'exécution de la commande !',
+            ephemeral: true,
+        });
+    }
+});
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs
@@ -34,8 +65,4 @@ for (const file of eventFiles) {
     }
 }
 
-
-
-// Le token permet à votre client de se connecter à Discord
 client.login(TOKEN);
-
